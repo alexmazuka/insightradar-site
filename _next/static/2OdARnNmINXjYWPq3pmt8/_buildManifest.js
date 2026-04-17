50523898442438eef1aa26e03cbba1fe0ae332c3
Oleksiy Matsuka <oleksiymatsuka@Alexs-MacBook-Pro.local>
2026-04-17 11:52:34 +0300

    Updates

--- _next/static/2OdARnNmINXjYWPq3pmt8/_buildManifest.js
+++ _next/static/2OdARnNmINXjYWPq3pmt8/_buildManifest.js
@@ -1,12 +1,7 @@
 self.__BUILD_MANIFEST = {
   "__rewrites": {
     "afterFiles": [],
-    "beforeFiles": [
-      {
-        "source": "/insightradar-site//_next/:path+",
-        "destination": "/insightradar-site/_next/:path+"
-      }
-    ],
+    "beforeFiles": [],
     "fallback": []
   },
   "sortedPages": [
